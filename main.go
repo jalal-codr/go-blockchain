99007edb64494e8c79f5acdf55010d17ceaefced
jalal-codr <adbuljalal849@gmail.com>
2024-11-12 22:18:28 +0100

    entry point defined

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
-	fmt.Println("Hello world")
+func main() {
+
+	bc := NewBlockChain()
+
+	bc.AddBlock("Block 1: Jalal just created the blockchain")
+
+	for _, block := range bc.blocks {
+
+		fmt.Println("Index", block.Index)
+		fmt.Println("Timestamp", block.Timestamp)
+		fmt.Println("Data", block.Data)
+		fmt.Println("PrvHash", block.PreviousHash)
+		fmt.Println("Hash", block.Hash)
+		fmt.Println()
+	}
+
 }
