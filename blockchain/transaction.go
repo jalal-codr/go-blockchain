4fb1f514b715282581c56fbc50fbf5ae82d17a90
jalal-codr <abduljalal849@gmail.com>
2024-11-16 21:29:20 +0100

    bug fix

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -10,3 +10,14 @@ func (bc *BlockChain) NewTransaction(to, from string, value float64) {
 	bc.Transactions = append(bc.Transactions, tx)
 
 }
+
+func (bc *BlockChain) CreatGenesisTransaction(to, from string, value float64) {
+	tx := &Transaction{
+		To:    to,
+		From:  from,
+		Value: value,
+	}
+	tx.ID = len(bc.Transactions) + 1
+	bc.Transactions = append(bc.Transactions, tx)
+
+}
